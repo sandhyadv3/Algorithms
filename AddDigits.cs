8a8e2a3f2df6db2253c7f83e41946aefd6875e15
Sandhyadv3 <sandhyadv3@gmail.com>
2015-10-06 22:36:25 -0700

    Add a number till becomes one digit.

---
+++ AddDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Algorithms
+{
+    public class AddDigits
+    {
+        public int AddTillOneDigit(int inputNumber)
+        {
+            if (inputNumber < 10)
+                return inputNumber;           
+
+           return AddTillOneDigit((inputNumber % 10) + (inputNumber / 10));
+        }
+    }   
+}
